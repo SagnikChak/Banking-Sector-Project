1548d6ad305c3abbf219e0931d97cd33b644a632
SagnikChak <55599414+SagnikChak@users.noreply.github.com>
2022-08-02 22:45:50 +0530

    Add files via upload

---
+++ src/banking_sector/Banking_Sector.java
@@ -0,0 +1,16 @@
+package banking_sector;
+
+public class Banking_Sector 
+{
+    public static void main(String[] args) 
+    {
+        new OpenProgress();
+        /*java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+        new OpenProgress().setVisible(true);
+        //new OpenProgress().dispose();
+        }
+        });*/
+    }
+    
+}
